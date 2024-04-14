8c602803c2798b9fc1a46970e54694f9c8b2e11d
jogit64 <usersfdc18@gmail.com>
2024-04-14 13:58:32 +0200

    Configure Redis for production 3

--- assistant.py
+++ assistant.py
@@ -19,7 +19,8 @@ app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'redis'
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_REDIS'] = redis.from_url('REDIS_URL')
+app.config['SESSION_REDIS'] = redis.from_url(os.environ.get('REDIS_URL'))
+
 
 Session(app)
 
