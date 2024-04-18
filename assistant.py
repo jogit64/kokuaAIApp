c91216d4cfc0e76093a8da94890a1f24a21b67fb
jogit64 <usersfdc18@gmail.com>
2024-04-18 17:29:25 +0200

    manque congif 03

--- assistant.py
+++ assistant.py
@@ -245,7 +245,7 @@ def handle_openai_request(gpt_config, messages_for_openai, conversation):
         top_p=gpt_config['top_p'],
         frequency_penalty=gpt_config['frequency_penalty'],
         presence_penalty=gpt_config['presence_penalty'],
-         instructions=gpt_config['instructions']
+        instructions=gpt_config['instructions']
     )
     response_chatgpt = chat_completion.choices[0].message.content
     app.logger.info(f"OpenAI Response: {response_chatgpt}")  # Log la réponse d'OpenAI
